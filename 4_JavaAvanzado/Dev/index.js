b72dd107cd69cc80b950910713cf9a22cfea48d6
AlexSmart7 <ajunior7.ac@gmail.com>
2025-02-13 18:45:46 -0600

    Actualizacion Dev_Nasa

--- 4_JavaAvanzado/Dev/index.js
+++ 4_JavaAvanzado/Dev/index.js
@@ -1,11 +1,13 @@
 var request = require('request');
+
 var options = {
-    'method': 'GET',
-    'url': 'https://api.nasa.gov/planetary/apod?api_key=TgVnwxOyTWiemrlxc4RkFm5v2f4yJO0lx02yeFI1',
-    'headers':{     
-    }
+  'method': 'GET',
+  'url': 'https://api.nasa.gov/planetary/apod?api_key=TgVnwxOyTWiemrlxc4RkFm5v2f4yJO0lx02yeFI1',
+  'headers': {
+  }
 };
-request(options,function(error,response){
-    if (error) throw new Error(error);
-    console.log(response.body);
+
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
 });
